5e457c001e82cca595e2251fb5ff113fd6184f47
Lucialva1 <alvarezaluciano@gmail.com>
2025-10-03 12:08:06 -0400

    Set up data constants for account.

--- escaperoom/src/main/java/com/escape/Model/DataConstants.java
+++ escaperoom/src/main/java/com/escape/Model/DataConstants.java
@@ -1,6 +1,11 @@
 package com.escape.Model;
 
 public class DataConstants {
-    protected static final String USER_ID = "user_id";
+    protected static final String USER_ID = "userId";
     protected static final String USER_FILE_NAME = "file";
+
+    protected static final String USER_USERNAME = "username";
+    protected static final String USER_PASSWORD = "password";
+    protected static final String USER_LEVEL = "level";
+    protected static final String USER_CURRENT_ROOM_ID = "currentRoomId";
 }
