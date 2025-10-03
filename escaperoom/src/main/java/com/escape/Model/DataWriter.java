dfc4f6d4f347782f90017942c2cba321862ce688
Soundlord0 <Soundlord0@gmail.com>
2025-10-03 14:18:20 -0400

    Created portion of DataWriter made Players.java

--- escaperoom/src/main/java/com/escape/Model/DataWriter.java
+++ escaperoom/src/main/java/com/escape/Model/DataWriter.java
@@ -1,5 +1,46 @@
 package com.escape.Model;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 public class DataWriter extends DataConstants {
+
+    public static void saveUsers() {
+        Players players = Players.getInstance();
+        ArrayList<Player> playerList = players.getPlayers();
+
+        JSONArray jsonUsers = new JSONArray();
+
+        for(int i=0; i<playerList.size(); i++) {
+            jsonUsers.add(getUserJSON(playerList.get(i)));
+        }
     
+
+        try (FileWriter file = new FileWriter(USER_TEMP_FILE_NAME)) {
+            file.write(jsonUsers.toJSONString());
+            file.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static JSONObject getUserJSON(Player player) {
+        JSONObject userDetails = new JSONObject();
+        userDetails.put(USER_ID, player.getId().toString());
+        userDetails.put(USER_USERNAME, player.getUserName());
+        userDetails.put(USER_PASSWORD, player.getPassword());
+        userDetails.put(USER_LEVEL, Integer.toString(player.getLevel()));
+        userDetails.put(User_CURRENT_ROOM_ID, player.getCurrentRoomID());
+    }
+    public static void saveGame() {
+        
+    }
+    public static void main(String[] args) {
+        DataWriter.saveUsers();
+        DataWriter.saveGame();
+    }
 }
