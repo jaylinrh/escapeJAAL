6719a676ffbf13463ae4560a9ee92310bf0f0990
Lucialva1 <alvarezaluciano@gmail.com>
2025-10-04 18:20:06 -0400

    Added USER_ARRAY constant. Fixed a couple syntax errors for DataLoader and added a tester method for the dataloader to use as we continue to add more data constants.

--- escaperoom/src/main/java/com/escape/Model/DataConstants.java
+++ escaperoom/src/main/java/com/escape/Model/DataConstants.java
@@ -4,6 +4,7 @@ public class DataConstants {
     protected static final String USER_ID = "userId";
     protected static final String USER_FILE_NAME = "json/users.json";
 
+    protected static final String USERS_ARRAY = "users";
     protected static final String USER_USERNAME = "username";
     protected static final String USER_PASSWORD = "password";
     protected static final String USER_LEVEL = "level";
--- escaperoom/src/main/java/com/escape/Model/DataLoader.java
+++ escaperoom/src/main/java/com/escape/Model/DataLoader.java
@@ -16,15 +16,15 @@ public class DataLoader extends DataConstants{
             try {
                 FileReader reader = new FileReader(USER_FILE_NAME);
                 JSONObject jsonObject = (JSONObject) new JSONParser().parse(reader);
-                JSONArray usersJSON = (JSONArray) jsonObject().get(USER_ARRAY);
+                JSONArray usersJSON = (JSONArray) jsonObject.get(USERS_ARRAY);
 
                 for(int i = 0; i <usersJSON.size(); i++) {
                     JSONObject userJSON = (JSONObject) usersJSON.get(i);
 
                     UUID id = UUID.fromString((String) userJSON.get(USER_ID));
                     String username = (String) userJSON.get(USER_USERNAME);
-                    String password = (String) usersJSON.get(USER_PASSWORD);
-                    int level = ((Long) userJSON.get(USER_LEVEL)).intvalue();
+                    String password = (String) userJSON.get(USER_PASSWORD);
+                    int level = ((Long) userJSON.get(USER_LEVEL)).intValue();
                     String currentRoomId = (String) userJSON.get(USER_CURRENT_ROOM_ID);
 
                     users.add(new Player(id, username, password, level, currentRoomId));
@@ -35,7 +35,32 @@ public class DataLoader extends DataConstants{
                 e.printStackTrace();
             }
 
-            return null;
+            return users;
         
     } 
+        /**
+         * This is a Tester method so I can see if the dataLoader is working for the current constants.
+         * @param args
+         */
+    public static void main(String[] args) {
+        System.out.println("Testing dataLoader with current constants \n");
+        ArrayList<Player> users = DataLoader.getUsers();
+
+        if(users == null || users.isEmpty()) {
+            System.out.println("error");
+        }  else {
+                System.out.println(" IT WORKS! " + users.size() + " users:\n");
+
+                for (int i = 0; i < users.size(); i++) {
+                    Player player = users.get(i);
+                    System.out.println(" User #" + (i+1) + ":");
+                    System.out.println(" ID: " + player.getId());
+                    System.out.println(" Username: " + player.getUserName());
+                    System.out.println(" Password: " + player.getPassword());
+                    System.out.println(" Level: " + player.getLevel());
+                    System.out.println(" Current Room: " + player.getCurrentRoomID());
+                }
+            }
+    
+    }
 }
