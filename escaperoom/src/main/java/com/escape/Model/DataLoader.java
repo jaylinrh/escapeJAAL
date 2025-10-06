80a1e6254cd06f4fe4a3264b4a4d1e8f23c72879
Lucialva1 <alvarezaluciano@gmail.com>
2025-10-06 14:36:35 -0400

    fixing merge conflict

--- escaperoom/src/main/java/com/escape/Model/DataLoader.java
+++ escaperoom/src/main/java/com/escape/Model/DataLoader.java
@@ -27,6 +27,51 @@ public class DataLoader extends DataConstants{
                     int level = ((Long) userJSON.get(USER_LEVEL)).intValue();
                     String currentRoomId = (String) userJSON.get(USER_CURRENT_ROOM_ID);
 
+                    JSONObject playerStateJSON = (JSONObject) userJSON.get(USER_PLAYER_STATE);
+
+                    int worldX = ((Long) playerStateJSON.get(PlayerState.WORLD_X)).intValue();
+                    int worldY = ((Long) playerStateJSON.get(PlayerState.WORLD_Y)).intValue();
+                    int speed = ((Long) playerStateJSON.get(PlayerState.SPEED)).intValue();
+                    String direction = (String) playerStateJSON.get(PlayerState.DIRECTION);
+                    boolean collisionOn = (Boolean) playerStateJSON.get(PlayerState.COLLISION_ON);
+
+                    JSONObject solidAreaJSON = (JSONObject) playerStateJSON.get(PlayerState.SOLID_AREA);
+
+                    int solidX = ((Long) solidAreaJSON.get(PlayerState.SolidArea.X)).intValue();
+                    int SolidY = ((Long) solidAreaJSON.get(PlayerState.SolidArea.Y)).intValue();
+                    int solidWidth = ((Long) solidAreaJSON.get(PlayerState.SolidArea.WIDTH)).intValue();
+                    int solidHeight = ((Long) solidAreaJSON.get(PlayerState.SolidArea.HEIGHT)).intValue();
+
+                    JSONObject spriteImagesJSON = (JSONObject) playerStateJSON.get(PlayerState.SPRITE_IMAGES);
+
+                    String u1 = (String) spriteImagesJSON.get(PlayerState.SpriteImages.U1);
+                    String u2 = (String) spriteImagesJSON.get(PlayerState.SpriteImages.U2);
+                    String d1 = (String) spriteImagesJSON.get(PlayerState.SpriteImages.D1);
+                    String d2 = (String) spriteImagesJSON.get(PlayerState.SpriteImages.D2);
+                    String l1 = (String) spriteImagesJSON.get(PlayerState.SpriteImages.L1);
+                    String l2 = (String) spriteImagesJSON.get(PlayerState.SpriteImages.L2);
+                    String r1 = (String) spriteImagesJSON.get(PlayerState.SpriteImages.R1);
+                    String r2 = (String) spriteImagesJSON.get(PlayerState.SpriteImages.R2);
+
+                    JSONObject inventoryJSON = (JSONObject) userJSON.get(USER_INVENTORY);
+                    String inventoryId = (String) inventoryJSON.get(Inventory.INVENTORY_ID);
+                    int maxCapacity = ((Long) inventoryJSON.get(Inventory.MAX_CAPACITY)).intValue();
+
+                   
+                    JSONArray itemsJSON = (JSONArray) inventoryJSON.get(Inventory.ITEMS);
+                    ArrayList<Item> items = new ArrayList<Item>();
+
+                    for (int j = 0; j < itemsJSON.size(); j++) {
+                        JSONObject itemJSON = (JSONObject) itemsJSON.get(j);
+                        String itemId = (String) itemsJSON.get(Inventory.Item.ITEM_ID);
+                        String itemName = (String) itemsJSON.get(Inventory.Item.NAME);
+                        String itemHint = (String) itemsJSON.get(Inventory.Item.HINT);
+                        String itemDesc = (String) itemJSON.get(Inventory.Item.DESCRIPTION);
+
+                        items.add(new Item(itemId, itemName, itemHint, itemDesc));
+                    }
+
+
                     users.add(new Player(id, username, password, level, currentRoomId));
                 } 
                 return users;
--- escaperoom/src/main/java/com/escape/Model/Player.java
+++ escaperoom/src/main/java/com/escape/Model/Player.java
@@ -10,7 +10,6 @@ public class Player {
 
 	
 	int currentLevel;
-	 String currentRoomId;
 	
 	public Player (String username, String password) {
 		this.username = username;
