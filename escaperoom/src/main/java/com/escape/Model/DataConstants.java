9077ba6799d3f4ac6c075289e6ce8e0fc5e396f4
Soundlord0 <Soundlord0@gmail.com>
2025-10-06 15:52:11 -0400

    unnested dataconstants

--- escaperoom/src/main/java/com/escape/Model/DataConstants.java
+++ escaperoom/src/main/java/com/escape/Model/DataConstants.java
@@ -16,44 +16,34 @@ public class DataConstants {
     protected static final String USER_PLAYER_STATE = "playerState";
     protected static final String USER_INVENTORY = "inventory";
 
-    public static class PlayerState {
-        public static final String WORLD_X = "worldX";
-        public static final String WORLD_Y = "worldY";
-        public static final String SPEED = "speed";
-        public static final String DIRECTION = "direction";
-        public static final String SOLID_AREA = "solidArea";
-        public static final String COLLISION_ON = "collisionOn";
-        public static final String SPRITE_IMAGES = "spriteImages";
+    public static final String WORLD_X = "worldX";
+    public static final String WORLD_Y = "worldY";
+    public static final String SPEED = "speed";
+    public static final String DIRECTION = "direction";
+    public static final String SOLID_AREA = "solidArea";
+    public static final String COLLISION_ON = "collisionOn";
+    public static final String SPRITE_IMAGES = "spriteImages";
 
-        public static class SolidArea {
-            public static final String X = "x";
-            public static final String Y = "y";
-            public static final String WIDTH = "width";
-            public static final String HEIGHT = "height";
-        }
+    public static final String X = "x";
+    public static final String Y = "y";
+    public static final String WIDTH = "width";
+    public static final String HEIGHT = "height";
 
-        public static class SpriteImages {
-            public static final String U1 = "u1";
-            public static final String U2 = "u2";
-            public static final String D1 = "d1";
-            public static final String D2 = "d2";
-            public static final String L1 = "l1";
-            public static final String L2 = "l2";
-            public static final String R1 = "r1";
-            public static final String R2 = "r2";
-        }
-    }
+    public static final String U1 = "u1";
+    public static final String U2 = "u2";
+    public static final String D1 = "d1";
+    public static final String D2 = "d2";
+    public static final String L1 = "l1";
+    public static final String L2 = "l2";
+    public static final String R1 = "r1";
+    public static final String R2 = "r2";
 
-    public static class Inventory {
-        public static final String INVENTORY_ID = "inventoryId";
-        public static final String MAX_CAPACITY = "maxCapacity";
-        public static final String ITEMS = "items";
+    public static final String INVENTORY_ID = "inventoryId";
+    public static final String MAX_CAPACITY = "maxCapacity";
+    public static final String ITEMS = "items";
 
-        public static class Item {
-            public static final String ITEM_ID = "itemID";
-            public static final String NAME = "name";
-            public static final String HINT = "hint";
-            public static final String DESCRIPTION = "description";
-        }
-    }
+    public static final String ITEM_ID = "itemID";
+    public static final String NAME = "name";
+    public static final String HINT = "hint";
+    public static final String DESCRIPTION = "description";
 }
