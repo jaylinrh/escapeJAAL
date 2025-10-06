70ae475002e972afd63f952fe3d1f0544f759913
CSE-Winston <wjiangli@email.sc.edu>
2025-10-06 00:12:20 -0400

    Added some other classes with basic attributes

---
+++ escaperoom/src/main/java/com/escape/Model/Puzzle.java
@@ -0,0 +1,10 @@
+package com.escape.Model;
+
+public class Puzzle {
+    private String puzzleId;
+    private String title;
+    private String description;
+    private int level;
+    private boolean isSolved;
+    
+}
---
+++ escaperoom/src/main/java/com/escape/Model/Room.java
@@ -0,0 +1,15 @@
+package com.escape.Model;
+
+import java.util.ArrayList;
+
+public class Room {
+    private String roomId;
+    private String name;
+    private String description;
+    private String mapFile;
+    private String music;
+    private Puzzle puzzle;
+    private Dialogue dialogue;
+    private ArrayList<String> availableItemIds;
+    
+}
